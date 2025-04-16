5813cec550ef30d5f7375fddc9ad9c944af4121f
k-hroma <rociomendonca@gmail.com>
2025-04-16 12:16:02 -0300

    links mod

--- app/src/utils/Links.jsx
+++ app/src/utils/Links.jsx
@@ -3,7 +3,7 @@ const Links = ({ className, url, text, children, newTag = false }) => {
   return (
   <>
       <a className={className} target={ newTag && "_blank" } href={url} rel={newTag && "noopener noreferrer"}>{text}</a>
-    <span>{children}</span>
+      <span>{children}</span>
   </>
   )
 }
